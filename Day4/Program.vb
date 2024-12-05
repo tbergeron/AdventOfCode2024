5dfafee7334077d58b3f831d5a73074a119595f4
Tommy Bergeron <tom@brainpad.org>
2024-12-05 17:53:29 +0800

    Day 4 Pt 2 Dirty but working!

--- Day4/Program.vb
+++ Day4/Program.vb
@@ -12,8 +12,6 @@ Sub Main(args As String())
     ' Get number of lines in input
     Dim maxY As Integer = input.Length
 
-    ' Console.WriteLine($"Max X: {maxX}, Max Y: {maxY}")
-
     ' Generate a two dimensional array of integers from the input
     Dim grid(maxX, maxY) As String
     For y As Integer = 0 To maxY - 1
@@ -31,6 +29,16 @@ Sub Main(args As String())
     Next
 
     Console.WriteLine($"Part 1: {part1Total}{vbCrLf}")
+
+    ' Loop through the grid and print out the contents
+    Dim part2Total As Integer = 0
+    For y As Integer = 0 To maxY - 1
+        For x As Integer = 0 To maxX - 1
+            part2Total += LookForMAS(x, y, grid)
+        Next
+    Next
+
+    Console.WriteLine($"Part 2: {part2Total}{vbCrLf}")
 End Sub
 
 Function LookForXMAS(x As Integer, y As Integer, grid As String(,)) As Integer
@@ -39,7 +47,6 @@ Function LookForXMAS(x As Integer, y As Integer, grid As String(,)) As Integer
     ' Forward
     If x + 3 < grid.GetLength(0) Then
         If grid(x, y) = "X" AndAlso grid(x + 1, y) = "M" AndAlso grid(x + 2, y) = "A" AndAlso grid(x + 3, y) = "S" Then
-            ' Console.WriteLine($"Forward Found: {x}, {y}")
             matches += 1
         End If
     End If
@@ -47,7 +54,6 @@ Function LookForXMAS(x As Integer, y As Integer, grid As String(,)) As Integer
     ' Backward
     If x - 3 >= 0 Then
         If grid(x, y) = "X" AndAlso grid(x - 1, y) = "M" AndAlso grid(x - 2, y) = "A" AndAlso grid(x - 3, y) = "S" Then
-            ' Console.WriteLine($"Backward Found: {x}, {y}")
             matches += 1
         End If
     End If
@@ -55,7 +61,6 @@ Function LookForXMAS(x As Integer, y As Integer, grid As String(,)) As Integer
     ' Upward
     If y - 3 >= 0 Then
         If grid(x, y) = "X" AndAlso grid(x, y - 1) = "M" AndAlso grid(x, y - 2) = "A" AndAlso grid(x, y - 3) = "S" Then
-            ' Console.WriteLine($"Upward Found: {x}, {y}")
             matches += 1
         End If
     End If
@@ -63,7 +68,6 @@ Function LookForXMAS(x As Integer, y As Integer, grid As String(,)) As Integer
     ' Downward
     If y + 3 < grid.GetLength(1) Then
         If grid(x, y) = "X" AndAlso grid(x, y + 1) = "M" AndAlso grid(x, y + 2) = "A" AndAlso grid(x, y + 3) = "S" Then
-            ' Console.WriteLine($"Downward Found: {x}, {y}")
             matches += 1
         End If
     End If
@@ -93,6 +97,53 @@ Function LookForXMAS(x As Integer, y As Integer, grid As String(,)) As Integer
     ' Downward Right
     If x + 3 < grid.GetLength(0) AndAlso y + 3 < grid.GetLength(1) Then
         If grid(x, y) = "X" AndAlso grid(x + 1, y + 1) = "M" AndAlso grid(x + 2, y + 2) = "A" AndAlso grid(x + 3, y + 3) = "S" Then
+            matches += 1
+        End If
+    End If
+
+    Return matches
+End Function
+
+
+Function LookForMAS(x As Integer, y As Integer, grid As String(,)) As Integer
+    Dim matches = 0
+
+    ' YX012
+    ' 0 M.M
+    ' 1 .A.
+    ' 2 S.S
+    If grid(x, y) = "M" AndAlso grid(x + 1, y + 1) = "A" AndAlso grid(x + 2 , y + 2) = "S" Then
+        If grid(x + 2, y) = "M" AndAlso grid(x + 1, y + 1) = "A" AndAlso grid(x, y + 2) = "S" Then
+            matches += 1
+        End If
+    End If
+
+    ' YX012
+    ' 0 M.S
+    ' 1 .A.
+    ' 2 M.S
+    If grid(x, y) = "M" AndAlso grid(x + 1, y + 1) = "A" AndAlso grid(x + 2 , y + 2) = "S" Then
+        If grid(x + 2, y) = "S" AndAlso grid(x + 1, y + 1) = "A" AndAlso grid(x, y + 2) = "M" Then
+            matches += 1
+        End If
+    End If
+
+    ' YX012
+    ' 0 S.M
+    ' 1 .A.
+    ' 2 S.M
+    If grid(x, y) = "S" AndAlso grid(x + 1, y + 1) = "A" AndAlso grid(x + 2 , y + 2) = "M" Then
+        If grid(x + 2, y) = "M" AndAlso grid(x + 1, y + 1) = "A" AndAlso grid(x, y + 2) = "S" Then
+            matches += 1
+        End If
+    End If
+
+    ' YX012
+    ' 0 S.S
+    ' 1 .A.
+    ' 2 M.M
+    If grid(x, y) = "S" AndAlso grid(x + 1, y + 1) = "A" AndAlso grid(x + 2 , y + 2) = "M" Then
+        If grid(x + 2, y) = "S" AndAlso grid(x + 1, y + 1) = "A" AndAlso grid(x, y + 2) = "M" Then
             matches += 1
         End If
     End If
